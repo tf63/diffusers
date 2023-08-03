2bb31173a7acd3b78b76960c2d6bec8422479b5d
tf63 <tfukuda9031@gmail.com>
2023-08-03 18:38:53 +0900

    feat: add stable diffusion demo

---
+++ work/scripts/stable_demo.py
@@ -0,0 +1,7 @@
+from diffusers import DiffusionPipeline
+import torch
+
+pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipeline.to("cuda")
+image = pipeline("a photo of tennis players").images[0]
+image.save("a.png")
